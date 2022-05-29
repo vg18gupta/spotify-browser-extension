95ea855a819a3270a767b4161c6e9de08f8dd1fa
Vishal Gupta <vg.vishal.gupta.10@gmail.com>
2022-05-29 19:32:16 +0530

    modifucations

--- popup/src/containers/App.js
+++ popup/src/containers/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react'
-import SpotifyLogo from '../components/SpotifyLogo'
-import Menubar from '../components/MenuBar'
-import ContentContainer from './ContentContainer'
+import React, { Component } from "react";
+import SpotifyLogo from "../components/SpotifyLogo";
+import Menubar from "../components/MenuBar";
+import ContentContainer from "./ContentContainer";
 
 class App extends Component {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  render() {
-
-    return (
-      <div className="container">
-        <SpotifyLogo />
-        {/* <Menubar /> */}
-        <ContentContainer />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="container" style={{ padding: "20px" }}>
+				<SpotifyLogo />
+				<Menubar />
+				<ContentContainer />
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
--- popup/src/index.js
+++ popup/src/index.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Root from './containers/Root'
-import '../style/index.less'
-import { Store, applyMiddleware } from 'react-chrome-redux'
-import thunkMiddleware from 'redux-thunk';
+import React from "react";
+import { render } from "react-dom";
+import Root from "./containers/Root";
+import "../style/index.less";
+import { Store, applyMiddleware } from "react-chrome-redux";
+import thunkMiddleware from "redux-thunk";
 
 const store = new Store({
-  portName: 'spotless'
-})
+	portName: "Spotify",
+});
 
 const middleware = [thunkMiddleware];
 const storeWithMiddleware = applyMiddleware(store, ...middleware);
 
 storeWithMiddleware.ready().then(() => {
-  render(
-    <Root store={store} />,
-    document.getElementById('root')
-  )
-})
+	render(<Root store={store} />, document.getElementById("root"));
+});
