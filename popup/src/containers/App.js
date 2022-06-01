6cdb7455ac0ce34001099d67e36325c278bb2d66
vg18gupta <vg.vishal.gupta.10@gmail.com>
2022-06-01 18:39:44 +0530

    padding added

--- popup/src/containers/App.js
+++ popup/src/containers/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
 
     return (
-      <div className="container">
+      <div className="container" style={{padding: "20px"}}>
         <SpotifyLogo />
         <Menubar />
         <ContentContainer />
